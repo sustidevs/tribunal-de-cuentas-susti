4bb6f781c0084df3ceeccc29b84684e67e90e418
Your Name <juancruzf017@gmail.com>
2021-11-08 17:07:16 -0300

    #9 update seeder areas

--- backend/database/seeders/AreaSeeder.php
+++ backend/database/seeders/AreaSeeder.php
@@ -13,27 +13,39 @@ class AreaSeeder extends Seeder
     Private $arrayAreas = array (
         array('descripcion' => 'SECRETARIA GENERAL TECNICA'),
         array('descripcion' => 'SECRETARIA GENERAL ADMINISTRATIVA'),
-        array('descripcion' => 'ASESORIA LETRADA'),
-        array('descripcion' => 'DIRECCION DE PERSONAL'),
-        array('descripcion' => 'DIRECCION DE ADMINISTRACION'),
-        array('descripcion' => 'DIRECCION DE REGISTRACIONES E INFORMATICA'),
-        array('descripcion' => 'VOCALIAS' )
+        //array('descripcion' => 'ASESORIA LETRADA'),
+        array('descripcion' => 'ASESORIA LEGAL'),
+        array('descripcion' => 'DIRECCIÓN DE PERSONAL'),
+        array('descripcion' => 'DEPARTAMENTO DE ADMINISTRACIÓN'),
+        array('descripcion' => 'DIRECCIÓN DE REGISTRACIONES'),
+        array('descripcion' => 'VOCALIA A'),
+        array('descripcion' => 'VOCALIA B'),
+        array('descripcion' => 'VOCALIA C'),
+        array('descripcion' => 'VOCALIA D')
     );
 
     Private $arraySubAreas = array (
-        array('descripcion' => 'Dpto. FALLOS'),
+        array('descripcion' => 'DEPARTAMENTO DE FALLOS'),
         array('descripcion' => 'ARCHIVO'),
-        array('descripcion' => 'PROSECRETARIA'),
-        array('descripcion' => 'Dpto. NOTIFICACION'),
-        array('descripcion' => 'MAYORDOMIA'),
-        array('descripcion' => 'Dpto. MESA DE ENTRADAS Y SALIDAS'),
-        array('descripcion' => 'Dpto. TESORERIA'),
-        array('descripcion' => 'Dpto. CONTABLE'),
-        array('descripcion' => 'Dpto. INFORMATICA'),
-        array('descripcion' => 'RELATORES'),
-        array('descripcion' => 'FISCALIZADORES'),
-        array('descripcion' => 'AUDITORES'),
-        array('descripcion' => 'AUXILIAR DE AUDITORIA')
+        //array('descripcion' => 'PROSECRETARIA'),
+        //array('descripcion' => 'MAYORDOMIA'),
+        array('descripcion' => 'DEPARTAMENTO MESA DE ENTRADA'),
+        array('descripcion' => 'DEPARTAMENTO DE NOTIFICACIONES'),
+        array('descripcion' => 'DIRECCIÓN DE INFORMATICA'),
+        //array('descripcion' => 'Dpto. TESORERIA'),
+        //array('descripcion' => 'Dpto. CONTABLE'),       
+        array('descripcion' => 'RELATORIA A'),
+        array('descripcion' => 'RELATORIA B'),
+        array('descripcion' => 'RELATORIA C'),
+        array('descripcion' => 'RELATORIA D'),
+        //array('descripcion' => 'FISCALIZADORES'),
+        array('descripcion' => 'AUDITORIA A'),
+        array('descripcion' => 'AUDITORIA B'),
+        array('descripcion' => 'AUDITORIA C'),
+        array('descripcion' => 'AUDITORIA D'),
+        array('descripcion' => 'PRESIDENCIA'),
+        array('descripcion' => 'MAESTRANZA'),
+        array('descripcion' => 'EQUIPO CTA DE INVERSIÓN')
         //último sin coma
         );
 
