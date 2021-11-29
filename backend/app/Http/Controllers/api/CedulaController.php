6d887bdf616f7dd5a3e8a377d4a27d2572af72ac
FernandezJuan017 <juancruzf017@gmail.com>
2021-11-29 13:00:14 -0300

    update CedulaController

--- backend/app/Http/Controllers/api/CedulaController.php
+++ backend/app/Http/Controllers/api/CedulaController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Models\Cedula;
+use App\Models\Expediente;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CedulaController extends Controller
 {
-    //
+    public function index()
+    {
+        $cedula = Cedula::index();
+        return response()->json($cedula,200);
+    }
+
+    public function create(Request $request)
+    {
+        $expedientes = Expediente::listadoExpedientes($request->user_id,3,3);//Mi Expedientes
+        $create_cedula = [$expedientes];
+        return response()->json($create_cedula,200);
+    }
+
+    public function store(Request $request)
+    {
+        $cedula = new Cedula;
+        $cedula->expediente_id = $request->expediente_id;
+        $cedula->descripcion = $request->descripcion;
+        $cedula->save();
+        return response()->json($cedula->getDatos(),200);
+    }
+
+    public function edit(Request $request)
+    {
+        $cedula = Cedula::findOrFaill($request->id);
+        $expediente = Expediente::listadoExpedientes($request->user_id,3,3);//Mi Expedientes
+        $edit_cedula = [$expedientes,$cedula->getDatos()];
+        return response()->json($edit_cedula,200);
+    }
+
+    public function update(Request $request)
+    {
+        $cedula = Cedula::findOrFaill($request->id);;
+        $cedula->expediente_id = $request->expediente_id;
+        $cedula->descripcion = $request->descripcion;
+        $cedula->update();
+        return response()->json($cedula->getDatos(),200);
+    }
 }
--- backend/app/Models/Cedula.php
+++ backend/app/Models/Cedula.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cedula extends Model
 {
@@ -11,7 +11,35 @@ class Cedula extends Model
 
     public function expediente()
     {
-        return $this->hasOne(Expediente::class);
+        return $this->belongsTo(Expediente::class);
+    }
+
+    public static function index()
+    {
+        $cedulas = Cedula::all();
+        $array_cedulas = collect([]);
+        foreach ($cedulas as $cedula)
+        {
+            $array_cedulas->push([
+                                'cedula_id' => $cedula->id,
+                                'nro_expediente' => $cedula->expediente->nro_expediente,
+                                'extracto' => $cedula->expediente->caratula->extracto->descripcion,
+                                'descripcion' => $cedula->descripcion
+                            ]);
+        }
+        return $array_cedulas;
+    }
+
+    public function getDatos()
+    {
+        $array_cedula = collect([]);
+        $array_cedula->push([
+                            'cedula_id' => $this->id,
+                            'nro_expediente' => $this->expediente->nro_expediente,
+                            'extracto' => $this->expediente->caratula->extracto->descripcion,
+                            'descripcion' => $this->descripcion
+                           ]);
+        return $array_cedula;
     }
 
 }
