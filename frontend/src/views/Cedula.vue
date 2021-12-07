5d48fd01c21a67c651f088bb6c0bae74e47785f9
Emiliano Cassoni <cassoniemiliano@gmail.com>
2021-12-07 12:56:03 -0300

    #97 conexion add cedula

--- frontend/src/views/Cedula.vue
+++ frontend/src/views/Cedula.vue
@@ -2,6 +2,7 @@
   <div>
     <titulo texto="Cedulas" icono="mdi-credit-card-edit"/>
     <div class="descripcion text-justify py-4">Si desea <strong>agregar una cedula</strong> a un expediente, haga clic en el botón de la tabla.</div>
+    <alert-sucess texto="La cedula ha sido cargada con exito" :condicion="this.cargado"/>
     <tabla-cedulas class="mb-15 pb-15" :headers="headers" :data="todos_expp" :loading="get_finalizado"/>
   </div>
 </template>
