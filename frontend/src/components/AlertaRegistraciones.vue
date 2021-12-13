e2f1a01ff80471dc3cab05de6e221693d60766be
Pruyas Araceli <pruyasaraceli@gmail.com>
2021-12-13 10:28:49 -0300

    #103 add alerta registraciones

---
+++ frontend/src/components/AlertaRegistraciones.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-alert
+      v-if="true"
+      icon="mdi-check"
+      dense
+      prominent
+      dismissible
+      text
+      type="info"
+      class="alert Montserrat-SemiBold mt-5 sizeAlert"
+      close-icon="mdi-close"
+  >
+    Han ingresado 5 expedientes con Motivo de SUBSIDIO y/o E/CTAS.APORTES NO REINTEGRABLE GASTOS
+    <strong class="mx-3" style="text-decoration: underline">Ver Expedientes</strong>
+  </v-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    texto: String,
+    condicion: Boolean,
+  }
+}
+</script>
+
+<style>
+.sizeAlert{
+  font-size: 16px !important;
+}
+</style>
