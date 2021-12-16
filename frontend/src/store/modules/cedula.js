1a82246fcc3ef36ffac9ab5028e7dcd8c06d4d25
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2021-12-16 09:46:11 -0300

    #123 cedula validacion

--- frontend/src/store/modules/cedula.js
+++ frontend/src/store/modules/cedula.js
@@ -3,11 +3,13 @@ import axios from "axios";
 const state = {
   cedula: "",
   cargado: false,
+  error_cedula: '',
 };
 
 const getters = {
-    getCedula: (state) => state.cedula,
+    getCedula: state => state.cedula,
     cargado: state => state.cargado,
+    get_error: state => state.error_cedula,
 };
 
 const actions = {
@@ -18,14 +20,18 @@ const actions = {
         console.log(response);
         commit('cargado', true)
         commit("set_cedula", response.data);
-      });
+      })
+      .catch(error => {
+        commit('set_error_cedula', error.response.data.errors.descripcion)
+    })
   },
 
 };
 
 const mutations = {
-  set_cedula: (state, cedula) => (state.cedula = cedula),
+  set_cedula: (state, cedula) => state.cedula = cedula,
   cargado: (state, cargado) => state.cargado = cargado,
+  set_error_cedula: (state, error_cedula) => state.error_cedula = error_cedula,
 };
 
 export default {
