5b167c408596b04c8ab0bf7790902a596bdca792
arapruy <72084746+arapruy@users.noreply.github.com>
2021-12-20 12:57:36 -0300

    #153 add conexion detalle

--- frontend/src/store/modules/cedula.js
+++ frontend/src/store/modules/cedula.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const state = {
-    cedula: "",
+    cedula: [],
     cargado: false,
     error_cedula: [],
     cedulas: [],
+    cedula_detalle: [],
 };
 
 const getters = {
+    get_detalle: state => state.cedula_detalle,
     getCedula: state => state.cedula,
     get_cargado: state => state.cargado,
     get_error: state => state.error_cedula,
@@ -15,6 +17,20 @@ const getters = {
 };
 
 const actions = {
+
+    verDetalle({commit},expediente_id) {
+        axios
+            .post(process.env.VUE_APP_API_URL + '/api/contar-cedula', expediente_id)
+            .then((response) => {
+                console.log(response.data)
+                commit('set_cargado', true)
+                commit("set_ceduladetalle", response.data);
+            })
+            .catch(error => {
+                commit('set_error_cedula', error.response.data.errors.descripcion)
+            })
+    },
+
     storeCedula({commit}, expediente) {
         axios
             .post(process.env.VUE_APP_API_URL + '/api/store-cedula', expediente)
@@ -34,6 +50,7 @@ const mutations = {
     set_cedulas: (state, cedulas) => state.cedulas = cedulas,
     set_cargado: (state, cargado) => state.cargado = cargado,
     set_error_cedula: (state, error_cedula) => state.error_cedula = error_cedula,
+    set_ceduladetalle: (state, cedula_detalle) => state.cedula_detalle = cedula_detalle,
 };
 
 export default {
