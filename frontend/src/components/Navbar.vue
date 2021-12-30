48c6633da68c2e54336110ceab93d3d2068c815d
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2021-12-30 11:15:37 -0300

    #179 update filtrado por áreas

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -8,35 +8,35 @@
             <a href="/"><LogoNavbar/></a>
           </v-col>
 
-            <v-col cols="12" lg="7" md="10" align="center" align-self="center">
-              <div v-if="get_user.area === 'DPTO. MESA DE ENTRADAS Y SALIDAS'" >
-                <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
-                <DropdownMenu titulo="Expedientes" :data-item="expedientesMesaEntrada"/>
-                <DropdownMenu titulo="Iniciadores" :data-item="iniciadores"/>
-              </div>
+          <v-col cols="12" lg="7" md="10" align="center" align-self="center">
+            <div v-if="get_user.area === 'DPTO. MESA DE ENTRADAS Y SALIDAS'" >
+              <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
+              <DropdownMenu titulo="Expedientes" :data-item="expedientesMesaEntrada"/>
+              <DropdownMenu titulo="Iniciadores" :data-item="iniciadores"/>
+            </div>
 
-              <div v-if="(get_user.area === 'VOCALIA A') || (get_user.area === 'VOCALIA B') || (get_user.area === 'VOCALIA C') || (get_user.area === 'VOCALIA D') || (get_user.area === 'RELATORIA A') || (get_user.area === 'RELATORIA B') || (get_user.area === 'RELATORIA C') || (get_user.area === 'RELATORIA D')" cols="12" lg="6" align="center" align-self="center">
-                <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
-                <DropdownMenu titulo="Expedientes" :data-item="expedientesRelatoriasyVocalias"/>
-              </div>
+            <div v-if="(get_user.area === 'VOCALIA A') || (get_user.area === 'VOCALIA B') || (get_user.area === 'VOCALIA C') || (get_user.area === 'VOCALIA D') || (get_user.area === 'RELATORIA A') || (get_user.area === 'RELATORIA B') || (get_user.area === 'RELATORIA C') || (get_user.area === 'RELATORIA D')" cols="12" lg="6" align="center" align-self="center">
+              <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
+              <DropdownMenu titulo="Expedientes" :data-item="expedientesRelatoriasyVocalias"/>
+            </div>
 
-              <div v-if="get_user.area === 'DIRECCIÓN DE REGISTRACIONES'" cols="12" lg="6" align="center" align-self="center">
-                <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
-                <DropdownMenu titulo="Expedientes" :data-item="expedientesMesaEntrada"/>
-                <DropdownMenu titulo="Iniciadores" :data-item="iniciadores"/>
-              </div>
+            <div v-if="get_user.area === 'DIRECCIÓN DE REGISTRACIONES'" cols="12" lg="6" align="center" align-self="center">
+              <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
+              <DropdownMenu titulo="Expedientes" :data-item="expedientesMesaEntrada"/>
+              <DropdownMenu titulo="Iniciadores" :data-item="iniciadores"/>
+            </div>
 
-              <div v-if="get_user.area === 'DIRECCIÓN DE INFORMATICA'" cols="12" lg="6" align="center" align-self="center">
-                <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
-                <DropdownMenu titulo="Expedientes" :data-item="expedientes"/>
-                <DropdownMenu titulo="Iniciadores" :data-item="iniciadores"/>
-              </div>
+            <div v-if="get_user.area === 'DIRECCIÓN DE INFORMATICA'" cols="12" lg="6" align="center" align-self="center">
+              <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
+              <DropdownMenu titulo="Expedientes" :data-item="expedientes"/>
+              <DropdownMenu titulo="Iniciadores" :data-item="iniciadores"/>
+            </div>
 
-              <div v-if="(get_user.area === 'SECRETARIA GENERAL TECNICA') || (get_user.area === 'SECRETARIA GENERAL ADMINISTRATIVA') || (get_user.area === 'ASESORIA LEGAL') || (get_user.area === 'DIRECCIÓN DE PERSONAL') || (get_user.area === 'DPTO. DE ADMINISTRACIÓN') || (get_user.area === 'DEPARTAMENTO FALLOS') || (get_user.area === 'ARCHIVO') || (get_user.area === 'DPTO. NOTIFICACIONES') || (get_user.area === 'DPTO. DE ADMINISTRACIÓN') || (get_user.area === 'AUDITORIA A') || (get_user.area === 'AUDITORIA B') || (get_user.area === 'AUDITORIA C') || (get_user.area === 'AUDITORIA D') || (get_user.area === 'PRESIDENCIA') || (get_user.area === 'MAESTRANZA') " cols="12" lg="6" align="center" align-self="center">
-                <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
-                <DropdownMenu titulo="Expedientes" :data-item="expedientes"/>
-              </div>
-            </v-col>
+            <div v-if="(get_user.area === 'SECRETARIA GENERAL TECNICA') || (get_user.area === 'SECRETARIA GENERAL ADMINISTRATIVA') || (get_user.area === 'ASESORIA LEGAL') || (get_user.area === 'DIRECCIÓN DE PERSONAL') || (get_user.area === 'DPTO. DE ADMINISTRACIÓN') || (get_user.area === 'DEPARTAMENTO FALLOS') || (get_user.area === 'ARCHIVO') || (get_user.area === 'DPTO. NOTIFICACIONES') || (get_user.area === 'DPTO. DE ADMINISTRACIÓN') || (get_user.area === 'AUDITORIA A') || (get_user.area === 'AUDITORIA B') || (get_user.area === 'AUDITORIA C') || (get_user.area === 'AUDITORIA D') || (get_user.area === 'PRESIDENCIA') || (get_user.area === 'MAESTRANZA')" cols="12" lg="6" align="center" align-self="center">
+              <DropdownMenu titulo="Bandejas" :data-item="bandejas"/>
+              <DropdownMenu titulo="Expedientes" :data-item="expedientes"/>
+            </div>
+          </v-col>
 
           <v-col cols="12" lg="2" md="1" align="end" align-self="center">
             <DropdownMenuPerfil />
@@ -62,9 +62,9 @@ export default {
 
   data: () => ({
     bandejas:[ {titulo:'Pendientes', link:'/expedientes-pendientes'},{titulo: 'Expedientes', link:'/expedientes'},{titulo: 'Historial Enviados',link:'/historial-enviados'}],
-    expedientes:[ {titulo: 'Nuevo Pase', link:'/mis-expedientes'}, {titulo: 'Recuperar', link:'/recuperar'}],
+    expedientes:[ {titulo: 'Pase', link:'/mis-expedientes'}, {titulo: 'Recuperar', link:'/recuperar'}],
     expedientesMesaEntrada:[ {titulo: 'Nuevo', link:'/nuevo-expediente'},{titulo: 'Pase', link:'/mis-expedientes'}, {titulo: 'Recuperar', link:'/recuperar'}, {titulo: 'Añadir cédula', link:'/cedula'}],
-    expedientesRelatoriasyVocalias:[ {titulo: 'Nuevo Pase', link:'/mis-expedientes'}, {titulo: 'Recuperar', link:'/recuperar'}, {titulo: 'Desglose', link:'/desglose'}, {titulo: 'Englose', link:'/englose'}],
+    expedientesRelatoriasyVocalias:[ {titulo: 'Pase', link:'/mis-expedientes'}, {titulo: 'Recuperar', link:'/recuperar'}, {titulo: 'Desglose', link:'/desglose'}, {titulo: 'Englose', link:'/englose'}],
     iniciadores:[  {titulo: 'Nuevo', link:'/nuevo-iniciador'} , {titulo: 'Ver todos', link:'/iniciadores'}],
   }),
 
