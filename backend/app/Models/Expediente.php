91afb4d95e498fa75b633385276a228581710b8c
Agustin <72084815+AgustinGM09@users.noreply.github.com>
2022-03-02 10:46:43 -0300

    funcionando

--- backend/app/Models/Expediente.php
+++ backend/app/Models/Expediente.php
@@ -334,6 +334,14 @@ class Expediente extends Model
                                                                 ->orderBy('hora', 'asc')
                                                                 ->get();                                                               
                         }
+                        if ($bandeja == 6) {
+                            return $historial_ultimo_movimiento ->where('area_destino_id', $user->area_id)
+                                                                ->whereIn('estado', [5,3])
+                                                                ->orderBy('prioridad', 'asc')
+                                                                ->orderBy('fecha', 'asc')
+                                                                ->orderBy('hora', 'asc')                                                                
+                                                                ->get();                                                             
+                        }
     }
 
     /**
