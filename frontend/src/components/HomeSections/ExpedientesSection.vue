8682366cdbdaf121ef319b9c1bd8242e573934fb
arypru <pruyasaraceli@gmail.com>
2022-03-02 09:25:48 -0300

    fix home buttons

--- frontend/src/components/HomeSections/ExpedientesSection.vue
+++ frontend/src/components/HomeSections/ExpedientesSection.vue
@@ -23,12 +23,8 @@
       <ButtonBigTooltip class="my-2 mr-6" texto="Pase" link="/mis-expedientes" icon="mdi-file-move" tooltip="Mis expedientes"/>
       <ButtonBig class="my-2 mr-6" texto="Recuperar" link="/recuperar" icon="mdi-file-undo"/>
       <ButtonBig v-if="(get_user.area === 'DPTO. MESA DE ENTRADAS Y SALIDAS')" class="my-2 mr-6" texto="Nuevo" link="/nuevo-expediente" icon="mdi-text-box-plus"/>
-
-      <div v-if="(get_user.area === 'DPTO. MESA DE ENTRADAS Y SALIDAS')">
-        <ButtonBig class="my-2 mr-6" texto="Englose" link="/englose" icon="mdi-file-plus"/>
-        <ButtonBig class="my-2 mr-6" texto="Desglose" link="/desglose" icon="mdi-file-percent"/>
-      </div>
-
+      <ButtonBig v-if="(get_user.area === 'DPTO. MESA DE ENTRADAS Y SALIDAS')" class="my-2 mr-6" texto="Englose" link="/englose" icon="mdi-file-plus"/>
+      <ButtonBig v-if="(get_user.area === 'DPTO. MESA DE ENTRADAS Y SALIDAS')" class="my-2 mr-6" texto="Desglose" link="/desglose" icon="mdi-file-percent"/>
   </div>
 </template>
 
